59ae9db48e5ab1a6900a9f91d8100b1fc6be1267
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2022-03-25 22:42:25 -0700

    Update interestingJS.js

--- interestingJS.js
+++ interestingJS.js
@@ -23,3 +23,23 @@ const gcd =(a,b) => (b===0) ? a : gcd(b, a%b);
 "asjdajskdjas1212".slice(0,-4). returns "asjdajskdjas" with last 4 chars sliced and "asjdajskdjas1212".slice(-4) return "1212", the last 4 characters
 ------------------------------------------------------------------------------------------------------------------------------------------------
 
+// FLatten Nested Array
+let arr = [ [ [ [1], 2], 3], [4], [], [[5]]]
+function flatten(nestedArray) {
+    // Your code here
+
+    return nestedArray.reduce((acc, cur) => acc.concat(Array.isArray(cur)? flatten(cur): cur), []);
+}
+
+Ans: [1, 2, 3, 4, 5]
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+// Remove Duplicates
+
+function removeDupes(str) {
+    // Your code here
+    return [...new Set([...str])].join('');
+}
+removeDupes('aabbccdd') returns 'abcd'
+
+------------------------------------------------------------------------------------------------------------------------------------------------
